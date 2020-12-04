4bbcb0378c0fc6b63dd0da460260e19d00c8ae84
krivoj <gabriele.canazza@gmail.com>
2020-12-04 02:41:43 +0100

    prince 2 demo

--- Demos/13-Prince of Persia 2/Unit1.pas
+++ Demos/13-Prince of Persia 2/Unit1.pas
@@ -17,13 +17,22 @@ type
     { Private declarations }
   public
     { Public declarations }
-  procedure Rectangle ( Name:string;X,Y,W,H: integer );
-  end;
+  procedure Rectangle ( Name:string;X,Y,W,H,FrameY: integer );
+  procedure CropTurn;
+  procedure CropTurnRunning;
+ end;
 
 var
   Form1: TForm1;
   prince2,aFrame,SpriteResult,SpriteTest: SE_Sprite;
   index: Integer;
+  FrameCount : array [0..1] of Integer;
+const
+  FrameWidth = 32;
+  FrameHeight = 40;
+  ANIM_Y_TURN = 0;
+  ANIM_Y_TURNRUNNING = 1;
+
 implementation
 
 {$R *.dfm}
@@ -47,55 +56,103 @@ begin
   SE_Background.CreateSprite(bmp.Bitmap,'back',{framesX}1,{framesY}1,{Delay}0,{X}SE_theater1.Width div 2,{Y}SE_theater1.Height div 2,{transparent}false,0);
   bmp.Free;
 
-  prince2:= SE_Engine1.CreateSprite('..\!media\prince2.bmp','prince2',{framesX}1,{framesY}1,{Delay}0,{X}SE_theater1.Width div 2,{Y}SE_theater1.Height div 2,{transparent}false,0);
+  prince2:= SE_Engine1.CreateSprite('..\!media\prince2.bmp','prince2',{framesX}1,{framesY}1,{Delay}0,{X}(SE_theater1.Width div 2)-200,{Y}SE_theater1.Height div 2,{transparent}false,0);
 
 
   SE_Theater1.Active := True;
 
-  bmp:= SE_Bitmap.Create(20*5,41);
+
+  // questo è il RESULT
+  bmp:= SE_Bitmap.Create(FrameWidth*9,FrameHeight*2);
   bmp.Bitmap.Canvas.Brush.Color := $00ff00;//clBlue;
   bmp.Bitmap.Canvas.FillRect( Rect(0,0,bmp.Width,bmp.Height));
-  SpriteResult := SE_Engine1.CreateSprite(bmp.Bitmap,'bmpresult',{framesX}1,{framesY}1,{Delay}300,{X}SE_theater1.Width div 2,{Y}20,{transparent}false,0);
+  SpriteResult := SE_Engine1.CreateSprite(bmp.Bitmap,'bmpresult',{framesX}1,{framesY}1,{Delay}2000,{X}(SE_theater1.Width div 2)+280,{Y}SE_theater1.Height div 2,{transparent}false,0);
   bmp.Free;
 
-  index :=0;
-  Rectangle ('turn',5,0,13,41);
-  inc(Index);
-  Rectangle ('turn',20,0,13,41);
-  inc(Index);
-  Rectangle ('turn',35,0,14,41);
-  inc(Index);
-  Rectangle ('turn',50,0,17,41);
-  inc(Index);
-  Rectangle ('turn',68,0,20,41);
-  inc(Index);
+  CropTurn;
+  FrameCount [ANIM_Y_TURN] := 9;
+  CropTurnRunning;
+  FrameCount [ANIM_Y_TURNRUNNING] := 9;
 
-  SpriteResult := SE_Engine1.CreateSprite(SpriteResult.BMP.Bitmap,'bmpresult',{framesX}5,{framesY}1,{Delay}120,{X}SE_theater1.Width div 2,{Y}90,{transparent}true,0);
+  //debug
+  SE_engine1.RemoveAllSprites('turn.');
+
+
+
+
+  SpriteTest := SE_Engine1.CreateSprite(SpriteResult.BMP.Bitmap,'bmpanim',{framesX}9,{framesY}2,{Delay}120,{X}SE_theater1.Width div 2,{Y}90,{transparent}true,0);
+  SpriteTest.BMP.Bitmap.SaveToFile('..\!media\princeSheet.bmp'  );
 
 end;
-procedure TForm1.Rectangle ( Name:string; X,Y,W,H: integer );
+procedure TForm1.Rectangle ( Name:string; X,Y,W,H,FrameY: integer );
 var
   bmp: SE_Bitmap;
 begin
-  prince2.BMP.CopyRectTo(SpriteResult.bmp,X,Y,(20*index) + ((20 - W) div 2),0,W+1,H+1,False,0);
+  prince2.BMP.CopyRectTo(SpriteResult.bmp,X,Y,(FrameWidth*index) + ((FrameWidth - W) div 2),FrameY*FrameHeight,W,H,False,0);
 
 
+  // singolo frame
   bmp:= SE_Bitmap.Create(W,H);
   prince2.BMP.CopyRectTo(bmp,X,Y,0,0,W,H,False,0);
-  aFrame:= SE_Engine1.CreateSprite(bmp.Bitmap,Name,{framesX}1,{framesY}1,{Delay}0,{X}X+W,{Y}20,{transparent}false,0);
+  aFrame:= SE_Engine1.CreateSprite(bmp.Bitmap,Name,{framesX}1,{framesY}1,{Delay}0,{X}(Index*10)+X+W,{Y}20,{transparent}false,0);
+  SE_Engine1.ProcessSprites(2000);
   bmp.Free;
-
-
 
 //  prince2.BMP.Canvas.MoveTo(X,Y);
 //  prince2.BMP.Canvas.LineTo(X+W,Y);
 //  prince2.BMP.Canvas.LineTo(X+W,Y+H);
 //  prince2.BMP.Canvas.LineTo(X,Y+H);
 //  prince2.BMP.Canvas.LineTo(X,Y);
-
-
-  //aFrame.ChangeBitmap(bmp.Bitmap,1,1,1);
+//aFrame.ChangeBitmap(bmp.Bitmap,1,1,1);
 
 
 end;
+procedure TForm1.CropTurn;
+begin
+
+  index :=0;
+  Rectangle ('turn.1',5,1,13,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.2',20,1,13,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.3',35,1,14,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.4',51,1,16,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.5',67,1,20,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.6',87,1,22,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.7',111,1,18,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.8',129,1,17,FrameHeight,0);
+  inc(Index);
+  Rectangle ('turn.9',148,1,16,FrameHeight,0);
+  inc(Index);
+
+end;
+procedure TForm1.CropTurnRunning;
+begin
+  index :=0;
+  Rectangle ('turnrunning.1',179,1,29,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.2',207,1,28,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.3',270,1,14,FrameHeight,1);    { TODO : continuare qui }
+  inc(Index);
+  Rectangle ('turnrunning.4',280,1,16,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.5',290,1,20,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.6',320,1,22,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.7',340,1,18,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.8',300,1,17,FrameHeight,1);
+  inc(Index);
+  Rectangle ('turnrunning.9',320,1,16,FrameHeight,1);
+  inc(Index);
+
+end;
+
 end.
